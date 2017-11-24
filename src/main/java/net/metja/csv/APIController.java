06bc027f1591b196e234bc85d14e572c0b308199
kborup@redhat.com <kborup@redhat.com>
2017-11-24 08:28:28 +0100

    java anoyed

--- src/main/java/net/metja/csv/APIController.java
+++ src/main/java/net/metja/csv/APIController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.*;
 
 import javax.xml.ws.Response;
 import java.io.File;
